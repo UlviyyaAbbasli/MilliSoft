db4fa9f1349d419e8476afd57d8964076294fd2c
UlviyyaAbbasli <ulviyyeqelenderli@gmail.com>
2024-03-17 21:01:22 +0400

    js hell edildi

---
+++ javascriptfirst/script.js
@@ -0,0 +1,159 @@
+// var eded=Number(prompt("eded daxil edin"))
+
+// if(eded % 2 == 0){
+//     console.log("eded cutdur:" + eded)
+// } else {
+//     console.log("eded tekdir:" + eded)
+// }
+
+
+
+
+
+// var a=Number(prompt("eded daxil edin"))
+// var b=Number(prompt("eded daxil edin"))
+// var c=Number(prompt("eded daxil edin"))
+
+// if(a>b && a>c){
+//     console.log("a boyukdur:" + a)
+// }
+// if(a<b && a<c){
+//     console.log("a kicikdir:" + a)
+// }
+// if(b>a && b>c){
+//     console.log("b boyukdur:" + b)
+// }
+// if(b<a && b<c){
+//     console.log("b kicikdir:" + b)
+// }
+// if(c>a && c>b){
+//     console.log("c boyukdur:" + c)
+// }
+// if(c<a && c<b){
+//     console.log("c kicikdir:" + c)
+// }
+
+
+
+
+// var cem=0;
+// var hasil=1;
+
+// for (i=1;i<=10;i++) {
+//     cem+=i;
+//     hasil*=i;
+// }
+// console.log(cem);
+// console.log(hasil);
+
+
+
+
+// var cem = 0;
+// var hasil = 1;
+
+// for (i = 1; i <= 100; i++){
+//     if (i % 2 == 0) {
+//         console.log(i);
+//         cem += i;
+//         hasil *= i;}
+        
+//     }
+
+// console.log(cem);
+// console.log(hasil);
+
+
+
+
+// for( var i=100; i<=999; i++){
+    
+//     if(i%56==0){
+//         console.log(i)
+//     }
+// }
+
+
+
+
+// var cem=0;
+// for( var i=100; i<=999; i++){
+//     if(i%56==0){
+//         cem+=i;
+//         console.log(cem);
+//     }
+// }
+
+
+
+
+// for( var i=100; i<=999; i++){
+//     if(i%56==0){
+//         console.log(i)
+//         break;
+//     }
+// }
+
+
+
+
+// for(var i=999; i>=100; i--){
+//     if(i%56==0){
+//         console.log(i);
+//         break;
+//     }
+// }
+
+
+
+
+// var a=Number(prompt("eded daxil edin"));
+// var b=Number(prompt("eded daxil edin"));
+// var c=prompt("riyazi emel daxil edin");
+
+// if(c=="+"){
+//     console.log(a+b)
+// }
+// else if(c=="-"){
+//     console.log(a-b)
+// }
+// else if(c=="*"){
+//     console.log(a*b)
+// }
+// else if(c=="/"){
+//     console.log(a/b)
+// }
+
+
+
+
+// var eded=Number(prompt("eded daxil edin"));
+
+// for(i=100; i<=999; i++){
+//     if(i%eded==0){
+//         console.log(i);
+//     }
+// }
+
+
+
+
+// var eded=Number(prompt("eded daxil edin"));
+
+// for(i=0; i<=eded; i++;){
+//     if(eded%i==0){
+//         console.log(i)
+//     }
+// }
+
+
+
+
+
+for(i=2; i<=j; i++){
+    for(j=2;i++;){
+        if(i%1==0 & i%j==0){
+            console.log(j)
+        }
+    }
+}
